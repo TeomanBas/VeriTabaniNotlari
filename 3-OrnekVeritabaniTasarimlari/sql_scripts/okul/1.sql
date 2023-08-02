50b929b9cf3da041377913e4e223a8c44f8eebe7
Teoman BAŞ <teomanbas@protonmail.com>
2023-08-02 12:08:38 +0300

    Düzeltmeler yapıldı.

--- 3-OrnekVeritabaniTasarimlari/sql_scripts/okul/1.sql
+++ 3-OrnekVeritabaniTasarimlari/sql_scripts/okul/1.sql
@@ -1,12 +1,19 @@
---database oluşturmak
+-- database oluşturmak
 
 CREATE SCHEMA `okul` 
 DEFAULT CHARACTER SET utf8 
 COLLATE utf8_unicode_ci ;
 
---ogreciler tablosu
+use okul;
+-- bolum tablosu
+CREATE TABLE bolumler(
+	bolum_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    bolum_ad VARCHAR(20)
+);
+
+-- ogreciler tablosu
 create table ogrenciler(
-	ogr_no int auto_increment not null unique primary key,
+	ogr_no int auto_increment not null primary key,
     ogr_ad varchar(30) not null,
     ogr_soyad varchar(30) not null,
     ogr_dogum_tarih datetime not null,
@@ -14,29 +21,22 @@ create table ogrenciler(
 	ogr_sehir varchar(20),
     ogr_bolum_no int
 )auto_increment=000001;
---auto_increment=000001; otomatik arttırma belirlenen sayıdan başlayarak artacak
---DATETIME YYYY-MM-DD HH:MM:SS
+
+-- auto_increment=000001; otomatik arttırma belirlenen sayıdan başlayarak artacak
+-- DATETIME YYYY-MM-DD HH:MM:SS
  
- --veri tabanı yönetim sistemlerinde constraint olarak isimlendirilen bazı zorlayıcı  ifadeler kullanılır.Bunlar
- null/not null, primary key,foreign key,check, unique, default dur.
- null ve not null:boş bırakılır veya boş bırakılamaz.
- foreign key: yabancıl anahtar
- primary key: birincil anahtar
- unique :tekil eşsiz benzersiz
- check:sütunlara girilecek olan değerlerin kısıtlanması olarak kullanılır.mesela not sütununda sadece 1 ile 100 arasında bir sayı olabilir.
- default: default 'yabancı' gibi herhangi bir veri girilmediğinde tablolarda yabancı olarak değer alacaktır.
+ -- veri tabanı yönetim sistemlerinde constraint olarak isimlendirilen bazı zorlayıcı  ifadeler kullanılır.Bunlar
+ -- null/not null, primary key,foreign key,check, unique, default dur.
+ -- null ve not null:boş bırakılır veya boş bırakılamaz.
+ -- foreign key: yabancıl anahtar
+ -- primary key: birincil anahtar
+ -- unique :tekil eşsiz benzersiz
+ -- check:sütunlara girilecek olan değerlerin kısıtlanması olarak kullanılır.mesela not sütununda sadece 1 ile 100 arasında bir sayı olabilir.
+ -- default: default 'yabancı' gibi herhangi bir veri girilmediğinde tablolarda yabancı olarak değer alacaktır.
  
 
 
-
-
- --bolum tablosu
-CREATE TABLE bolumler(
-	bolum_id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
-    bolum_ad VARCHAR(20)
-)
-
---dersler tablosu
+-- dersler tablosu
 CREATE TABLE dersler(
 ders_id INT AUTO_INCREMENT PRIMARY KEY,  
 fk_bolum_id INT NOT NULL,
@@ -44,7 +44,7 @@ ders_ad VARCHAR(50),
 FOREIGN KEY (fk_bolum_id) REFERENCES bolumler(bolum_id)
 )
 
---NOTLAR TABLOSU
+-- NOTLAR TABLOSU
 CREATE TABLE notlar(
 ogr_no INT NOT NULL,
 ders_id INT NOT NULL,
@@ -54,12 +54,4 @@ CONSTRAINT chkvize CHECK (vize>=0 AND vize<=100),
 CONSTRAINT chkfinal CHECK (final>=0 AND final<=100),
 CONSTRAINT pkkey PRIMARY KEY(ogr_no,ders_id)
 )ENGINE=INNODB;
-
-
-
-
-
-
-
-
 
